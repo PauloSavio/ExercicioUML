b1ef40796228d2bb8ab6d0e7b9a13a008f817fa6
Savio <pauloluizsavio@gmail.com>
2021-09-23 14:16:31 -0300

    Rasteiros adicionados

---
+++ src/br/com/zup/Rasteiros.java
@@ -0,0 +1,22 @@
+package br.com.zup;
+
+public class Rasteiros extends Inimigos{
+    private double danoEspinhos;
+
+    public Rasteiros(String nome, double vida, double danoEspinhos) {
+        super(nome, vida);
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    public double getDanoEspinhos() {
+        return danoEspinhos;
+    }
+
+    public void setDanoEspinhos(double danoEspinhos) {
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    public double aplicarDano(double dano){
+        return dano;
+    }
+}
