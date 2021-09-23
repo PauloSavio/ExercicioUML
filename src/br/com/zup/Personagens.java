71f69d8b1c925ddf163f5ef2a92fa661c6d6ccf5
Savio <pauloluizsavio@gmail.com>
2021-09-23 13:58:39 -0300

    Heroi adicionado

--- src/br/com/zup/Personagens.java
+++ src/br/com/zup/Personagens.java
@@ -1,2 +1,27 @@
-package br.com.zup;public class Personagens {
+package br.com.zup;
+
+public class Personagens {
+    private String nome;
+    private int vida;
+
+    public Personagens(String nome, int vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
 }
