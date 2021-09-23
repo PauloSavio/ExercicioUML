8d2d5c4061b14dc925596cbb7532a7ad9e2b243c
Savio <pauloluizsavio@gmail.com>
2021-09-23 14:14:54 -0300

    Guerreiros adicionados

---
+++ src/br/com/zup/Guerreiros.java
@@ -0,0 +1,22 @@
+package br.com.zup;
+
+public class Guerreiros extends Heroi{
+    private double poderAtaque;
+
+    public Guerreiros(String nome, double vida, double poderAtaque) {
+        super(nome, vida);
+        this.poderAtaque = poderAtaque;
+    }
+
+    public double getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public void setPoderAtaque(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
+
+    public double aplicarDano(double dano){
+        return dano;
+    }
+}
